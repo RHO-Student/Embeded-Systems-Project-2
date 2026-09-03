96bb58090bff3fdab8b832986658de059dc89551
Rory Henry-Osborne <HENRR4@student.op.ac.nz>
2026-09-03 16:33:17 +1200

    I did not cook at 2 am and i am paying for it

--- Assessment_2_Arduino_code/Assessment_2_Arduino_code.ino
+++ Assessment_2_Arduino_code/Assessment_2_Arduino_code.ino
@@ -4,18 +4,29 @@ int offset = 0;
 
 
 void setup() {
-  serial.begin(9600);
-  serial.printIn("Pausing to warm up sensor");
+  Serial.begin(9600);
+  Serial.print("Pausing to warm up sensor");
   delay(120000); //sensor needs between 2-5 min for a mq-2
-  serial.printIn("Sensor warming done");
+  Serial.print("Sensor warming done");
 }
 
 void loop() {
-  // int Control = anagougeRead(control_MQ2);
-  // int rawContam = anagougeRead(Contaminated_MQ2);
-  // int offset = rawContam - Control; //this is for if you are normilising the snesor in clean air 
-  // int ajustedContam = rawContam - offset;
-  // int diff = ajustedContam - Control;
-  // float diffVolt = diff *(5.0/1023.0); 
-  // return all variables with temp then timestamp with system time
+  int Control = analogRead(control_MQ2);
+  int rawContam = analogRead(Contaminated_MQ2);
+  int offset = rawContam - Control; //this is for if you are normilising the snesor in clean air 
+  int ajustedContam = rawContam - offset;
+  int diff = ajustedContam - Control;
+  float diffVolt = diff *(5.0/1023.0); 
+  Serial.print(Control);
+  Serial.print(",");
+  Serial.print(rawContam);
+  Serial.print(",");
+  Serial.print(offset);
+  Serial.print(",");
+  Serial.print(ajustedContam);
+  Serial.print(",");
+  Serial.print(diff);
+  Serial.print(",");
+  Serial.print(diffVolt);
+  //return all variables with temp then timestamp with system time
 }
--- serial_controler.py
+++ serial_controler.py
@@ -1,37 +1,17 @@
+# https://www.w3schools.com/python/python_file_open.asp
+
 import serial
-import struct
 import time
 
-PACKET_FORMAT = "<HHHHff" #i need 4 16bit ints (raw sensor, control, offset, diff) and two floats (calculated diff and temp)
-PACKET_SIZE = struct.calcsize(PACKET_FORMAT) #16bytes
-
-uno = serial.Serial('') # Update port to match your Pi (/dev/ttyACM0 or /dev/ttyUSB0), 9600, timeout=1)
+uno = serial.Serial('',9600,timeout=1) # Update port to match your Pi (/dev/ttyACM0 or /dev/ttyUSB0), 9600, timeout=1)
 time.sleep(4) #timeout to allow connection
-
-def request_data():
-    uno.reset_input_buffer()
-    uno .write(b'R')
-    
-    raw_bytes = uno.read(PACKET_SIZE)
-    if len(raw_bytes) == PACKET_SIZE: #change to match raw data
-        sensor1, sensor2, temp, status = struct.unpack(PACKET_FORMAT, raw_bytes)
-        return {
-            "sensor1": sensor1,
-            "sensor2": sensor2,
-            "temperature": round(temp, 2),
-            "status": status
-        }
-    else:
-        raise TimeoutError("Bad data received")
 
 def main():
     req = True
-    while req:
-        try:
-            data = request_data()
-            print(f"Received via Serial: {data}")
-        except Exception as e:
-            print(f"Error: {e}")
-            time.sleep(1)
-        print("Do you want to request more data")
-        req = input ## wrong fix latter
+    with open("","+a",encoding="utf-8") as csv:  
+        while req:
+            if uno.in_waiting > 0:
+                text = uno.readline().decode('utf-8').rstrip
+                csv.write(text + "\n")
+                data = text.split(',')
+                print(f"Receved {data}")
