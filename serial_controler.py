98b00abcfdbcd3f9f55e03dffa077b052dbb7655
Rory Henry-Osborne (1000067845) <HENRR4@student.op.ac.nz>
2026-09-07 13:55:18 +1200

    2/3 parts working yippie

--- serial_controler.py
+++ serial_controler.py
@@ -5,21 +5,23 @@ import time
 
 def main():
     print ("NOTE there is a minute between each reading entered")
-    amount = int(input("How many readings do you want? "))
-    for i in range (amount):
-        uno = serial.Serial('COM3',9600,timeout=1) # Update port to match your Pi (/dev/ttyACM0 or /dev/ttyUSB0), 9600, timeout=1) COM3 for win usuly
-        time.sleep(2)
-        with open(r"D:\Embeded-Systems-Project-2\Readings.csv","a",encoding="utf-8") as csv:  # dispite working relitivly at home abslute is needed for runing at course
-            uno.write(b'1')
-            for i in range(5) :
-                    text = uno.readline().decode('utf-8').rstrip()
-                    if (text != ""):
-                        systime = time.localtime()
-                        formtime = time.strftime("%d-%m-%Y %H:%M",systime)
-                        csv.write("\n" + text + "," + formtime)
-                    data = text.split(',')
-                    print(f"Received {data}")
-#excess data is not saved to the csv so having it loop 5 times is good for catching slow serial stuff 
-    time.sleep(60)
 
-main()
+    uno = serial.Serial('COM3',9600,timeout=1) # Update port to match your Pi (/dev/ttyACM0 or /dev/ttyUSB0), 9600, timeout=1) COM3 for win usuly
+    time.sleep(2)
+    with open(r"H:\Embeded-Systems-Project-2\Readings.csv","a",encoding="utf-8") as csv:  # dispite working relitivly at home abslute is needed for runing at course 
+        uno.write(b'1')
+        for i in range(5) :
+            text = uno.readline().decode('utf-8').rstrip()
+            if (text != ""):
+                systime = time.localtime()
+                formtime = time.strftime("%d-%m-%Y %H:%M",systime)
+                csv.write("\n" + text + "," + formtime)
+            data = text.split(',')
+            print(f"Received {data}")
+#excess data is not saved to the csv so having it loop 5 times is good for catching slow serial stuff 
+
+
+amount = int(input("How many readings do you want? "))
+for i in range (amount):
+    main()
+    time.sleep(60)
