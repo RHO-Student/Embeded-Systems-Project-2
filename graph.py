d5cdd1733e3adb6b3d5f0095b503fedb511e34b7
Rory Henry-Osborne <HENRR4@student.op.ac.nz>
2026-09-07 15:01:05 +1200

    Web app started making

--- graph.py
+++ graph.py
@@ -4,5 +4,17 @@ import streamlit
 import pandas
 
 streamlit.title("Embedded Systems Sensor project")
-data = pandas.read_csv()# add file
-streamlit.dataframe(data)
+streamlit.markdown("---")
+data = pandas.read_csv(r"C:\Users\roars\Pictures\cODE SHEET\Embeded-Systems-Project-2\Readings.csv")# add file
+streamlit.subheader("Gas sensor readings")
+streamlit.dataframe(data,width="content", height="auto")
+col1, col2, col3 = streamlit.columns(3)
+with col1:
+    streamlit.metric(label="Total Logged Data Points", value=len(data))
+with col2:
+    if 'Con' in data.columns:
+        streamlit.metric(label="Max Concentration (Con)", value=f"{data['Con'].max()}")
+with col3:
+    if 'diff' in data.columns:
+        streamlit.metric(label="Avg Sensor Deviation (diff)", value=f"{data['diff'].mean():.2f}")
+streamlit.markdown("---")
